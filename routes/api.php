dfdb066cc95de1a79c50d4ef6ecc05c4c99142cf
vitrasethy <sethy.vitra@gmail.com>
2024-03-19 20:38:53 +0700

    add new routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\SportTypeController;
 use App\Http\Controllers\VenueController;
+use App\Models\Announcement;
+use App\Models\MatchGame;
+use App\Models\Team;
+use App\Models\TempRecruitment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,3 +17,7 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 Route::apiResource('sport-types', SportTypeController::class);
 Route::apiResource('venues', VenueController::class);
 Route::apiResource('reservation', ReservationController::class);
+Route::apiResource('teams', Team::class);
+Route::apiResource('announcements', Announcement::class);
+Route::apiResource('match-games', MatchGame::class);
+Route::apiResource('temp-recruitments', TempRecruitment::class);
