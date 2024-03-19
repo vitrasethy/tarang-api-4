34c7b17e86ea112c926f3e5de7dc6080621c6987
vitrasethy <sethy.vitra@gmail.com>
2024-03-19 20:40:50 +0700

    fix added new routes

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
+use App\Http\Controllers\AnnouncementController;
+use App\Http\Controllers\MatchGameController;
 use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\SportTypeController;
+use App\Http\Controllers\TeamController;
+use App\Http\Controllers\TempRecruitmentController;
 use App\Http\Controllers\VenueController;
-use App\Models\Announcement;
-use App\Models\MatchGame;
-use App\Models\Team;
-use App\Models\TempRecruitment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,7 +17,7 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 Route::apiResource('sport-types', SportTypeController::class);
 Route::apiResource('venues', VenueController::class);
 Route::apiResource('reservation', ReservationController::class);
-Route::apiResource('teams', Team::class);
-Route::apiResource('announcements', Announcement::class);
-Route::apiResource('match-games', MatchGame::class);
-Route::apiResource('temp-recruitments', TempRecruitment::class);
+Route::apiResource('teams', TeamController::class);
+Route::apiResource('announcements', AnnouncementController::class);
+Route::apiResource('match-games', MatchGameController::class);
+Route::apiResource('temp-recruitments', TempRecruitmentController::class);
