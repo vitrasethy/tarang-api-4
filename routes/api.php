ea699c5ff4ae3667c62db715b79a367cd8a64a13
vitrasethy <sethy.vitra@gmail.com>
2024-03-29 10:14:15 +0700

    update is login.

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\TeamController;
 use App\Http\Controllers\TempRecruitmentController;
 use App\Http\Controllers\VenueController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
@@ -16,8 +17,10 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 });
 
 Route::get('is-login', function () {
-    return response()->status();
-})->middleware(['auth:sanctum']);
+    if (Auth::check()){
+        return response()->json(['message' => 'Authorized']);
+    }else return response()->json(['message' => 'Unauthorized']);
+});
 
 Route::apiResource('sport-types', SportTypeController::class);
 Route::apiResource('venues', VenueController::class);
