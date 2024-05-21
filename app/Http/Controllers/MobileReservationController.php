5cb4a89e6966b52ce5cf0e4e3e89f17c3d31d604
vitrasethy <sethy.vitra@gmail.com>
2024-05-21 15:18:17 +0700

    add filter to match game

--- app/Http/Controllers/MobileReservationController.php
+++ app/Http/Controllers/MobileReservationController.php
@@ -12,9 +12,23 @@ class MobileReservationController extends Controller
         $reservations = Reservation::latest()->get();
         $resource = [];
         foreach ($reservations as $reservation) {
-            $venue = Venue::find($reservation->id);
-            $resource = [
-                ...$reservation,
+            $venue = Venue::find($reservation->venue_id); // Corrected to use $reservation->venue_id instead of $reservation->id
+
+            $resource[] = [
+                'id' => $reservation->id,
+                'phone' => $reservation->phone,
+                'user_id' => $reservation->user_id,
+                'venue_id' => $reservation->venue_id,
+                'team_id' => $reservation->team_id,
+                'attendee' => $reservation->attendee,
+                'date' => $reservation->date,
+                'start_time' => $reservation->start_time,
+                'end_time' => $reservation->end_time,
+                'find_team' => $reservation->find_team,
+                'find_member' => $reservation->find_member,
+                'deleted_at' => $reservation->deleted_at,
+                'created_at' => $reservation->created_at,
+                'updated_at' => $reservation->updated_at,
                 'venue_name' => $venue->name,
             ];
         }
