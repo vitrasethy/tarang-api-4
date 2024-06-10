6e4bd1fd96ab65610cf94f7cbfeae65ecabafffe
vitrasethy <sethy.vitra@gmail.com>
2024-06-10 08:32:31 +0700

    policy 2 days before reservation

--- app/Http/Controllers/GetAvailablesTimeController.php
+++ app/Http/Controllers/GetAvailablesTimeController.php
@@ -17,7 +17,7 @@ class GetAvailablesTimeController extends Controller
     {
         $validated = $request->validated();
 
-        if (Carbon::parse($validated['date'].' '.$validated['start_time'])->diffInMinutes(now()) >= -30) {
+        if (Carbon::parse($validated['date'].' '.$validated['start_time'])->diffInMinutes(now()) >= -2880) {
             return response()->json([
                 'message' => "Can't reserve 30mn before.",
             ], 404);
