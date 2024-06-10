1793e3b30e830d4ba71b1d82849a6f14eb55276b
vitrasethy <sethy.vitra@gmail.com>
2024-06-10 16:06:54 +0700

    test sms.

--- app/Notifications/SendReminderSMS.php
+++ app/Notifications/SendReminderSMS.php
@@ -3,10 +3,11 @@
 namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\VonageMessage;
 use Illuminate\Notifications\Notification;
 
-class SendReminderSMS extends Notification
+class SendReminderSMS extends Notification implements ShouldQueue
 {
     use Queueable;
 
